4eaefb8b3be485ed4a6da518a3faf8839fc0ef92
GUSTAVO FERNANDO AGUILAR FLORES <gaguilarf@unsa.edu.pe>
2025-12-27 15:08:47 -0500

    refactor(TextFooter, ValentinesProposal): remove copyright notice and update image file extensions

--- src/components/TextFooter.tsx
+++ src/components/TextFooter.tsx
@@ -22,12 +22,6 @@ export default function TextFooter() {
       >
         para revelar <br /> <span className="text-gray-400">la sorpresa</span>
       </h1>
-
-      {/* Copyright */}
-      <p className="absolute bottom-5 right-5 text-white text-sm opacity-15 hover:opacity-50">
-        © {new Date().getFullYear()}{" "}
-        <Link href="https://visibait.com">visibait.com</Link>
-      </p>
     </>
   );
 }
